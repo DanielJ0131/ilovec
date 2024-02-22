681355a003984908441cd4051355659d70d3198f
Joe Goldberg <zxlol298@gmail.com>
2024-02-22 23:18:48 +0100

    remove redundancies

--- src/dice/dice.c
+++ src/dice/dice.c
@@ -3,11 +3,52 @@
 #include <time.h>
 #include <unistd.h>
 
-void menu() {
+void clearScreen() {
+    // ANSI escape code clear the screen
+    printf("\033[2J\033[H");
+}
+
+void printHeader() {
+    // ANSI escape code set text color to cyan
+    printf("\033[1;36m");
+
+    // Print a stylized title
+    printf("+-------------------------+\n");
+    printf("|   Cool 3D Dice Menu    |\n");
+    printf("+-------------------------+\n");
+
+    // ANSI escape code reset text color
+    printf("\033[0m");
+}
+
+void printMenu() {
+    // ANSI escape code set text color to green
+    printf("\033[1;32m");
+
     printf("1. Roll the dice\n");
     printf("2. Exit\n");
-    if (getchar() == '2') {
-        exit(0);
+
+    printf("\033[0m");
+}
+
+void menu() {
+    clearScreen();
+    printHeader();
+    printMenu();
+
+    char choice;
+    printf("Enter your choice: ");
+    scanf(" %c", &choice);
+
+    switch (choice) {
+        case '1':
+            printf("Rolling the dice...\n");
+            break;
+        case '2':
+            exit(0);
+            break;
+        default:
+            printf("Invalid choice. Please try again.\n");
     }
 }
 
@@ -15,35 +56,31 @@ int rollDice() {
     return (rand() % 6) + 1;
 }
 
-void clearTerminal() {
-    system("clear"); // Use "cls" for Windows
-}
-
 void animation() {
     struct timespec sleepTime;
     sleepTime.tv_sec = 0;
     sleepTime.tv_nsec = 250000000; // 250 milliseconds
 
-    clearTerminal();
+    clearScreen();
     printf("Rolling the dice\n");
     nanosleep(&sleepTime, NULL);
-    clearTerminal();
+    clearScreen();
 
     printf("Rolling the dice.\n");
     nanosleep(&sleepTime, NULL);
-    clearTerminal();
+    clearScreen();
 
     printf("Rolling the dice..\n");
     nanosleep(&sleepTime, NULL);
-    clearTerminal();
+    clearScreen();
 
     printf("Rolling the dice...\n");
     nanosleep(&sleepTime, NULL);
-    clearTerminal();
+    clearScreen();
 
     printf("Rolling the dice....\n");
     nanosleep(&sleepTime, NULL);
-    clearTerminal();
+    clearScreen();
 
 }
 
@@ -125,14 +162,14 @@ void drawDice(int number) {
 
 }
 
-void main() {
+int main(void) {
     menu();
     while (1) {
         animation();
         int result = rollDice();
         printf("The dice rolled: %d!\n", result);
         drawDice(result);
-        sleep(1);
-        clearTerminal();
+        sleep(2);
+        clearScreen();
     }
 }
