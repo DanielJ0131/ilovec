c678d03abd55b0d675c991fe00ae01e2dd71e499
DanielJ0131 <daniel.jonsson0131@stud.hkr.se>
2024-02-24 13:15:08 +0100

    Bug fix

--- src/lyricguesser/lyricguesser.c
+++ src/lyricguesser/lyricguesser.c
@@ -2,29 +2,63 @@
 
 #define MAX_LENGTH 1000
 
+// Taken from dice/dice.c
+void clearScreen() {
+    // ANSI escape code clear the screen
+    printf("\033[2J\033[H");
+}
 
-void menu() {
+void printHeader() {
+    // ANSI escape code set text color to cyan
+    printf("\033[1;36m");
+
+    // Print a stylized title
+    printf("+-------------------------+\n");
+    printf("|   Cool 3D Dice Menu    |\n");
+    printf("+-------------------------+\n");
+
+    // ANSI escape code reset text color
+    printf("\033[0m");
+}
+
+void printMenu() {
+    // ANSI escape code set text color to green
+    printf("\033[1;32m");
+
     printf("1. Play\n");
     printf("2. Exit\n");
+
+    printf("\033[0m");
+}
+
+void menu() {
+    clearScreen();
+    printHeader();
+    printMenu();
+
+    char choice;
     printf("Enter your choice: ");
-    char choice = getchar();
+    scanf(" %c", &choice);
 
     switch (choice) {
         case '1':
-            printf("Playing the game...\n");
             break;
         case '2':
-            printf("Exiting the game...\n");
+            exit(0);
             break;
         default:
-            printf("Invalid choice.\n");
+            printf("Invalid choice. Please try again.\n");
+            sleep(2);
+            menu();
             break;
     }
 }
 
 
+
 int main() {
     menu();
+
     FILE *file = fopen("lyrics.txt", "r");
     if (file == NULL) {
         printf("Failed to open the file.\n");
