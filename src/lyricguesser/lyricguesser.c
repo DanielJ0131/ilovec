92278afdeba25e556f6c0824fbef0c88e8e72490
DanielJ0131 <daniel.jonsson0131@stud.hkr.se>
2024-02-24 12:37:06 +0100

    Bug fix

--- src/lyricguesser/lyricguesser.c
+++ src/lyricguesser/lyricguesser.c
@@ -2,7 +2,29 @@
 
 #define MAX_LENGTH 1000
 
+
+void menu() {
+    printf("1. Play\n");
+    printf("2. Exit\n");
+    printf("Enter your choice: ");
+    char choice = getchar();
+
+    switch (choice) {
+        case '1':
+            printf("Playing the game...\n");
+            break;
+        case '2':
+            printf("Exiting the game...\n");
+            break;
+        default:
+            printf("Invalid choice.\n");
+            break;
+    }
+}
+
+
 int main() {
+    menu();
     FILE *file = fopen("lyrics.txt", "r");
     if (file == NULL) {
         printf("Failed to open the file.\n");
